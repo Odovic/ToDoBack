60926e00f6064d408dd6942541c539f722f75236
Milan.Zuza <milan.zuza18@gmail.com>
2023-07-28 18:21:04 +0200

    err midleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
 import express  from "express";
+import errorMiddleware from "./middleware/error-middleware";
 const app=express();
 
+
+app.use(errorMiddleware);
 app.listen(process.env.PORT,()=>{
     console.log(`Server is listening on port ${process.env.PORT}`);
 })
