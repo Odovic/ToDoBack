4a921051aef4c787094a506138cdb0e8b8696f6b
Milan.Zuza <milan.zuza18@gmail.com>
2023-07-28 19:00:46 +0200

    added cors package

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
+import cors from "cors";
 import express  from "express";
 import errorMiddleware from "./middleware/error-middleware";
 import userRouter from "./routes/user-router"
 
 const app=express();
+app.use(express.json());
+app.use(cors());
 
 app.use("/user",userRouter);
 
