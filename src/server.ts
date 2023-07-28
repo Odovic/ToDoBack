8867872b2a11cf8616c3c5b913c190281c85ba68
Milan.Zuza <milan.zuza18@gmail.com>
2023-07-28 17:44:34 +0200

    initial setup

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import express  from "express";
+const app=express();
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is listening on port ${process.env.PORT}`);
+})
